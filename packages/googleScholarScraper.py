c56abc1a79d31bcb374feab8a1d9b211945ae9f8
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-06-21 23:05:26 -0600

    space

--- packages/googleScholarScraper.py
+++ packages/googleScholarScraper.py
@@ -7,7 +7,7 @@ class ArticleScraper:
     """
 
     def __init__(self):
-        self. headers = {
+        self.headers = {
             'User-agent':
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
             }

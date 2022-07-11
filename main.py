2a30f9d106df51ed2fe8f70f8ad66af01ef17c45
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-07-10 18:50:08 -0600

    added presaved files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, redirect, url_for, render_template, request, session
 from packages import googleScholarScraper
+import pickle
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -14,6 +16,7 @@ app.config['SECRET_KEY'] = '12345'
 @app.route('/', methods=['GET'])
 def index(**kwargs):
     topics = ['Requirements engineering', 'NLP', 'Artificial Intelligence']
+    topics_dir_path = 'static\Files\topics_dict.pickle'
 
     scraper = googleScholarScraper.ArticleScraper()
     topics_dict = {}
@@ -21,6 +24,12 @@ def index(**kwargs):
     for topic in topics:
         dict_ = scraper.scrape_topic(phrase=topic)
         topics_dict[topic] = dict_
+
+    # read the saved file in case the scraping didn't work
+    if not topics_dict:
+        with open(topics_dir_path, 'wb') as f:
+            pickle.dump(topics_dict, f)
+    else: topics_dict = pd.read_pickle(topics_dir_path)
 
     return render_template('index.html', topics_dict=topics_dict)
 
@@ -30,9 +39,16 @@ def publications(**kwargs):
 
     # Ann Barcomb user id 
     user = "1hMBs-8AAAAJ"
+    topics_dir_path = 'static\Files\publications_list.pickle'
 
     scraper = googleScholarScraper.ArticleScraper()
     publications_list = scraper.scrape(user=user)
+
+    # read the saved file in case the scraping didn't work
+    if not publications_list:
+        with open(topics_dir_path, 'wb') as f:
+            pickle.dump(publications_list, f)
+    else: publications_list = pd.read_pickle(topics_dir_path)
     
     return render_template('publications.html', publications_list=publications_list)
 
